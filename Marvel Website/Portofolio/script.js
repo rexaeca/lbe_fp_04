4ba5cfa6c3482cfd3c4b44c8d539c0b97951fa83
Marvel Timothy Noya <marveltimnoya@gmail.com>
2026-09-23 18:37:46 +0700

    Delete Marvel Website/Portofolio directory

--- Marvel Website/Portofolio/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Set Tahun Dinamis
-    const yearElement = document.getElementById('year');
-    if (yearElement) {
-        yearElement.textContent = new Date().getFullYear();
-    }
-    
-    console.log("Monochrome portfolio loaded successfully.");
-});
